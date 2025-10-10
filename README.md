193ec5ce51be8810425952275f2154b46b468a34
Matheus Mendes <mendes.souza.mat@gmail.com>
2025-10-10 03:33:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# matheus-mendes-api
+Gerenciador de produtos químicos para dedetização. Objeto de estudos para pós graduação MIT em Arquitetura de Software. Projeto para a turma Arquitetura em Java 25E4
---
+++ src/test/java/br/edu/infnet/matheus_mendes_api/MatheusMendesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.matheus_mendes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheusMendesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
